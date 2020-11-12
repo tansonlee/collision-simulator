e611a664b9669b53ed82a8e13dc4d6593b435a05
Joseph Perez <josephperez@Josephs-MacBook-Pro.local>
2020-11-12 14:07:21 -0500

    added ramp

--- sketch.js
+++ sketch.js
@@ -3,9 +3,10 @@ let ball2;
 
 function setup() {
 	createCanvas(600, 400);
-	ball1 = new Ball(100, 220);
+	ball1 = new Ball(100, 260);
 	ball2 = new Ball(500, 180);
 	ball1.vel.x = 4;
+	ball1.vel.y = 1
 	ball2.vel.x = -4;
 }
 
@@ -15,4 +16,6 @@ function draw() {
 	ball2.update();
 	ball1.render();
 	ball2.render();
+	fill(255, 253, 208);
+	triangle(0, 250, 600, 400, 0, 400);
 }

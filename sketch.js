900a0209e6ccb15cd0022fb86508553d7a360714
Joseph Perez <perezj0230@gmail.com>
2020-12-27 20:57:55 -0500

    added reset and start buttons

--- sketch.js
+++ sketch.js
@@ -2,30 +2,43 @@ let ball1;
 let ball2;
 let ballDisplay1;
 let ballDisplay2;
+let initialPos1;
+let initialPos2;
+let initialVelocity1;
+let initialVelocity2;
 let elasticCollision;
 let twoD;
+let resetBalls;
+let b;
 
 function setup() {
     const cnv = createCanvas(1200, 600);
-    elasticCollision = false;
-    twoDCollision = true;
+    elasticCollision = true;
+    twoDCollision = false;
     cnv.style("display", "block");
-    ball1 = new Ball(500, 150, "Ball 1", 10);
-    ball2 = new Ball(640, 100, "Ball 2", 10);
-    // ball1 = new Ball(200, 500, "Ball 1", 10);
-    // ball2 = new Ball(500, 200, "Ball 2", 10);
-    ball1.vel.x = 10;
-    ball1.vel.y = -8;
-    ball2.vel.x = 0;
-    ball2.vel.y = 0;
+    ball1 = new Ball(0, 0, "Ball 1", 10);
+    ball2 = new Ball(0, 0, "Ball 2", 10);
+    initialPos1 = createVector(200, 200);
+    initialPos2 = createVector(500, 150);
+    initialVelocity1 = createVector(10, -8);
+    initialVelocity2 = createVector(1, 0);
+    resetBalls = true;
     ball1.acc.y = 0;
     ball2.acc.y = 0;
     ballDisplay1 = new DisplayBox(ball1, width - 300, 20);
     ballDisplay2 = new DisplayBox(ball2, width - 150, 20);
-    b = createButton("click me");
+    b = createButton("reset");
+    b.mousePressed(changeReset);
+    b2 = createButton("start");
+    b2.mousePressed(changeVelocities);
 }
 
 function draw() {
+    if (resetBalls) {
+        setInitialStates();
+        b2 = createButton("start");
+        b2.mousePressed(changeVelocities);
+    } else b2.hide();
     background(241, 250, 238);
     if (!twoDCollision) {
         fill(62);
@@ -49,3 +62,18 @@ function draw() {
         }
     }
 }
+
+const setInitialStates = () => {
+    ball1.pos = initialPos1.copy();
+    ball2.pos = initialPos2.copy();
+};
+
+const changeReset = () => {
+    resetBalls = true;
+};
+
+const changeVelocities = () => {
+    ball1.vel = initialVelocity1;
+    ball2.vel = initialVelocity2;
+    resetBalls = false;
+};

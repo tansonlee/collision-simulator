a91c18dba4b441862611490934ab9e00bd17a642
tansonlee <leetanson@gmail.com>
2020-12-29 21:54:57 -0500

    refactored and styled buttons

--- ball.js
+++ ball.js
@@ -1,62 +1,50 @@
 class Ball {
-    constructor(x, y, name, mass, ballColor) {
-        this.pos = createVector(x, y); // createVector is equivalent to PVector in processing
-        this.mass = mass;
-        this.vel = createVector(0, 0);
-        this.acc = createVector(0, 0);
-        this.d = 75; // diameter
-        this.name = name;
-        this.ballColor = ballColor;
-    }
+	constructor(pos, vel, name, mass, ballColor) {
+		this.pos = pos;
+		this.mass = mass;
+		this.vel = vel;
+		this.d = 75; // diameter
+		this.name = name;
+		this.ballColor = ballColor;
+	}
 
-    render() {
-        //stroke(0, 90, 25);
-        fill(this.ballColor);
-        ellipse(this.pos.x, this.pos.y, this.d, this.d);
-        this.arrow(0, 0, true);
-    }
+	render() {
+		//stroke(0, 90, 25);
+		fill(this.ballColor);
+		ellipse(this.pos.x, this.pos.y, this.d, this.d);
+		this.arrow(0, 0, true);
+	}
 
-    update() {
-        this.vel.add(this.acc);
-        this.pos.add(this.vel);
-        // this.wrapEdges();
-    }
+	update() {
+		this.pos.add(this.vel);
+	}
 
-    wrapEdges() {
-        if (this.pos.x < 0 || this.pos.x > width) {
-            this.vel.x *= -1;
-        }
-        if (this.pos.y < 0 || this.pos.y > height) {
-            this.vel.y *= -1;
-        }
-    }
-
-    arrow(posx, posy, dynamic) {
-        const start = createVector(posx, posy);
-        const end = this.vel.copy().setMag(30);
-        if (dynamic) {
-            // whether arrow should follow ball
-            start.add(this.pos);
-        }
-        if (this.vel.mag() === 0) {
-            fill(0);
-            ellipse(start.x, start.y, 7, 7);
-        } else {
-            drawArrow(start, end, "black");
-        }
-    }
+	arrow(posx, posy, dynamic) {
+		const start = createVector(posx, posy);
+		const end = this.vel.copy().setMag(30);
+		if (dynamic) {
+			// whether arrow should follow ball
+			start.add(this.pos);
+		}
+		if (this.vel.mag() === 0) {
+			fill(0);
+			ellipse(start.x, start.y, 7, 7);
+		} else {
+			drawArrow(start, end, "black");
+		}
+	}
 }
 
 const drawArrow = (base, vec, myColor) => {
-    push();
-    stroke(myColor);
-    strokeWeight(3);
-    fill(myColor);
-    translate(base.x, base.y);
-    line(0, 0, vec.x, vec.y);
-    rotate(vec.heading());
-    let arrowSize = 7;
-    translate(vec.mag() - arrowSize, 0);
-    triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
-    pop();
+	push();
+	stroke(myColor);
+	strokeWeight(3);
+	fill(myColor);
+	translate(base.x, base.y);
+	line(0, 0, vec.x, vec.y);
+	rotate(vec.heading());
+	let arrowSize = 7;
+	translate(vec.mag() - arrowSize, 0);
+	triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+	pop();
 };
--- sketch.js
+++ sketch.js
@@ -1,84 +1,133 @@
-let ball1;
-let ball2;
-let ballDisplay1;
-let ballDisplay2;
-let initialPos1;
-let initialPos2;
-let initialVelocity1;
-let initialVelocity2;
+let ball1, ball2;
+let ballDisplay1, ballDisplay2;
+
+let initialPos1, initialPos2;
+let initialVelocity1, initialVelocity2;
+
 let elasticCollision = true;
 let twoDCollision = true;
-let resetBalls = true;
-let b;
-let b2;
 let collisionOccurred = false;
 
+let resetButton, startButton;
+
+let updateBallsFlag = false;
+
 function setup() {
-    const cnv = createCanvas(1200, 600);
-    cnv.style("display", "block");
-    const color1 = color(230, 57, 70);
-    const color2 = color(66, 135, 245);
-    ball1 = new Ball(0, 0, "Ball 1", 10, color1);
-    ball2 = new Ball(0, 0, "Ball 2", 10, color2);
-    initialPos1 = createVector(200, 360);
-    initialPos2 = createVector(500, 150);
-    initialVelocity1 = createVector(10, -8);
-    initialVelocity2 = createVector(1, 0);
-    ball1.vel = initialVelocity1;
-    ball2.vel = initialVelocity2;
-    resetBalls = true;
-    ball1.acc.y = 0;
-    ball2.acc.y = 0;
-    ballDisplay1 = new DisplayBox(ball1, width - 300, 20);
-    ballDisplay2 = new DisplayBox(ball2, width - 150, 20);
-    b = createButton("reset");
-    b.mousePressed(changeReset);
-    b2 = createButton("start");
-    b2.mousePressed(changeVelocities);
+	const cnv = createCanvas(1200, 600);
+	cnv.style("display", "block");
+
+	const canvasWrapper = document.getElementById("canvas-wrapper");
+	cnv.parent(canvasWrapper);
+
+	initializeBalls();
+	initializeDisplays();
+	initializeButtons();
 }
 
 function draw() {
-    if (resetBalls) {
-        setInitialStates();
-        b2.show();
-    } else b2.hide();
-    background(241, 250, 238);
-    if (!twoDCollision) {
-        fill(62);
-        rect(0, 337.5, 1200, 300);
-        ball1.vel.y = 0;
-        ball2.vel.y = 0;
-        ball1.pos.y = 300;
-        ball2.pos.y = 300;
-    }
-    ball1.update();
-    ball2.update();
-    ball1.render();
-    ball2.render();
-    ballDisplay1.render();
-    ballDisplay2.render();
-    if (isColliding(ball1, ball2) && !collisionOccurred) {
-        if (elasticCollision) {
-            setElasticVelocities(ball1, ball2);
-        } else {
-            setInelasticVelocities(ball1, ball2);
-        }
-        collisionOccurred = true;
-    }
+	background(241, 250, 238);
+
+	// if (resetBalls) {
+	// 	setInitialStates();
+	// 	startButton.show();
+	// } else startButton.hide();
+
+	if (!twoDCollision) {
+		setupTwoD();
+	}
+
+	drawBalls();
+	if (updateBallsFlag) {
+		updateBalls();
+	}
+	drawDisplayBoxes();
+
+	if (isColliding(ball1, ball2) && !collisionOccurred) {
+		if (elasticCollision) {
+			setElasticVelocities(ball1, ball2);
+		} else {
+			setInelasticVelocities(ball1, ball2);
+		}
+		collisionOccurred = true;
+	}
 }
 
 const setInitialStates = () => {
-    ball1.pos = initialPos1.copy();
-    ball2.pos = initialPos2.copy();
-    ball1.vel = initialVelocity1.copy();
-    ball2.vel = initialVelocity2.copy();
-    collisionOccurred = false;
+	ball1.pos = initialPos1.copy();
+	ball2.pos = initialPos2.copy();
+	ball1.vel = initialVelocity1.copy();
+	ball2.vel = initialVelocity2.copy();
+	collisionOccurred = false;
 };
 
-const changeReset = () => {
-    resetBalls = true;
+const handleReset = () => {
+	setInitialStates();
+	startButton.show();
+	updateBallsFlag = false;
 };
 
-const changeVelocities = () => {
-    resetBalls = false;
+const handleStart = () => {
+	startButton.hide();
+	// ball1.vel = initialVelocity1.copy();
+	// ball2.vel = initialVelocity2.copy();
+	updateBallsFlag = true;
+};
+
+const initializeBalls = () => {
+	const ball1Color = color(230, 57, 70);
+	const ball2Color = color(66, 135, 245);
+
+	initialPos1 = createVector(200, 360);
+	initialPos2 = createVector(500, 150);
+
+	initialVelocity1 = createVector(10, -8);
+	initialVelocity2 = createVector(1, 0);
+
+	ball1 = new Ball(null, null, "Ball 1", 10, ball1Color);
+	ball2 = new Ball(null, null, "Ball 2", 10, ball2Color);
+
+	setInitialStates();
+};
+
+const initializeDisplays = () => {
+	ballDisplay1 = new DisplayBox(ball1, width - 300, 20);
+	ballDisplay2 = new DisplayBox(ball2, width - 150, 20);
+};
+
+const initializeButtons = () => {
+	resetButton = createButton("reset");
+	resetButton.mousePressed(handleReset);
+	resetButton.addClass("reset-button");
+
+	startButton = createButton("start");
+	startButton.mousePressed(handleStart);
+	startButton.addClass("start-button");
+
+	const buttonWrapper = document.getElementById("button-wrapper");
+	resetButton.parent(buttonWrapper);
+	startButton.parent(buttonWrapper);
+};
+
+const setupTwoD = () => {
+	fill(62);
+	rect(0, 337.5, 1200, 300);
+	ball1.vel.y = 0;
+	ball2.vel.y = 0;
+	ball1.pos.y = 300;
+	ball2.pos.y = 300;
+};
+
+const drawBalls = () => {
+	ball1.render();
+	ball2.render();
+};
+
+const updateBalls = () => {
+	ball1.update();
+	ball2.update();
+};
+
+const drawDisplayBoxes = () => {
+	ballDisplay1.render();
+	ballDisplay2.render();
 };

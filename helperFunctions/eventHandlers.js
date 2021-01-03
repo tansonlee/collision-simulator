c7472e281de552eb9902177bb0590ec4c53fc2a5
Joseph Perez <perezj0230@gmail.com>
2021-01-02 21:46:02 -0500

    added 2d and elastic button functionality

--- helperFunctions/eventHandlers.js
+++ helperFunctions/eventHandlers.js
@@ -1,13 +1,21 @@
 const handleReset = () => {
-	setInitialStates();
-	startButton.show();
-	updateBallsFlag = false;
+    setInitialStates();
+    startButton.show();
+    updateBallsFlag = false;
 };
 
 const handleStart = () => {
-	startButton.hide();
-	updateBallsFlag = true;
-	collisionOccurred = false;
-	initialPos1 = ball1.pos.copy();
-	initialPos2 = ball2.pos.copy();
+    startButton.hide();
+    updateBallsFlag = true;
+    collisionOccurred = false;
+    initialPos1 = ball1.pos.copy();
+    initialPos2 = ball2.pos.copy();
+};
+
+const handleTwoD = () => {
+    twoDCollision = !twoDCollision;
+};
+
+const handleElastic = () => {
+    elasticCollision = !elasticCollision;
 };
